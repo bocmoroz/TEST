effad7ac65998003f401a6c4106a63d0cd1cd202
Andrey Mokeev <mokeevandrey1996@gmail.com>
2022-11-14 10:14:37 +0600

    adjusted data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,39 @@
-CREATE TABLE IF NOT EXISTS public.product
+CREATE SEQUENCE IF NOT EXISTS product_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE SEQUENCE IF NOT EXISTS warehouse_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE SEQUENCE IF NOT EXISTS warehouse_income_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE SEQUENCE IF NOT EXISTS warehouse_sale_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE SEQUENCE IF NOT EXISTS warehouse_transportation_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS product
 (
     id bigint NOT NULL DEFAULT nextval('product_id_seq'::regclass),
     articul character varying(255) COLLATE pg_catalog."default",
@@ -7,118 +42,117 @@ CREATE TABLE IF NOT EXISTS public.product
     last_income_price bigint,
     last_sale_price bigint,
     name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT product_pkey PRIMARY KEY (id),
-    CONSTRAINT uk_d2en73jto4x7mryn4actf91sk UNIQUE (articul)
+    CONSTRAINT product_id_pkey PRIMARY KEY (id),
+    CONSTRAINT product_articul_key UNIQUE (articul)
 );
 
-CREATE TABLE IF NOT EXISTS public.warehouse
+CREATE TABLE IF NOT EXISTS warehouse
 (
     id bigint NOT NULL DEFAULT nextval('warehouse_id_seq'::regclass),
     deleted boolean NOT NULL,
     last_change_date timestamp without time zone,
     name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT warehouse_pkey PRIMARY KEY (id),
-    CONSTRAINT uk_dbmkeyi4co3vmnwnjxoocd4nh UNIQUE (name)
+    CONSTRAINT warehouse_id_pkey PRIMARY KEY (id),
+    CONSTRAINT warehouse_name_key UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS public.warehouse_products_count
+CREATE TABLE IF NOT EXISTS warehouse_products_count
 (
     warehouse_id bigint NOT NULL,
     count integer,
     product_id bigint NOT NULL,
-    CONSTRAINT fk483fdyvkfqw6hpngvh965bpbu FOREIGN KEY (warehouse_id)
-        REFERENCES public.warehouse (id) MATCH SIMPLE
+    CONSTRAINT warehouseProductsCount_warehouseId_fkey FOREIGN KEY (warehouse_id)
+        REFERENCES warehouse (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT fkc203lyujxb74fo4y6gncj74gt FOREIGN KEY (product_id)
-        REFERENCES public.product (id) MATCH SIMPLE
+    CONSTRAINT warehouseProductsCount_productId_fkey FOREIGN KEY (product_id)
+        REFERENCES product (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.warehouse_income
+CREATE TABLE IF NOT EXISTS warehouse_income
 (
     id bigint NOT NULL DEFAULT nextval('warehouse_income_id_seq'::regclass),
     create_date timestamp without time zone,
     warehouse_id bigint,
-    CONSTRAINT warehouse_income_pkey PRIMARY KEY (id),
-    CONSTRAINT fkktlb6jkbqf5jpf0yb7rqx0f01 FOREIGN KEY (warehouse_id)
-        REFERENCES public.warehouse (id) MATCH SIMPLE
+    CONSTRAINT warehouseIncome_id_pkey PRIMARY KEY (id),
+    CONSTRAINT warehouseIncome_warehouseId_fkey FOREIGN KEY (warehouse_id)
+        REFERENCES warehouse (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.income_products_count
+CREATE TABLE IF NOT EXISTS income_products_count
 (
     product_income_id bigint NOT NULL,
     count integer,
     product_id bigint NOT NULL,
-    CONSTRAINT fk2bnof4jebfd1rdky7gn8478sf FOREIGN KEY (product_id)
-        REFERENCES public.product (id) MATCH SIMPLE
+    CONSTRAINT incomeProductsCount_productId_fkey FOREIGN KEY (product_id)
+        REFERENCES product (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT fkhx3dcid8q0g14555ffhfyg1ca FOREIGN KEY (product_income_id)
-        REFERENCES public.warehouse_income (id) MATCH SIMPLE
+    CONSTRAINT incomeProductsCount_productIncomeId_fkey FOREIGN KEY (product_income_id)
+        REFERENCES warehouse_income (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.warehouse_sale
+CREATE TABLE IF NOT EXISTS warehouse_sale
 (
     id bigint NOT NULL DEFAULT nextval('warehouse_sale_id_seq'::regclass),
     create_date timestamp without time zone,
     warehouse_id bigint,
-    CONSTRAINT warehouse_sale_pkey PRIMARY KEY (id),
-    CONSTRAINT fkgdvrrdsa42iqrriwudbqs16tg FOREIGN KEY (warehouse_id)
-        REFERENCES public.warehouse (id) MATCH SIMPLE
+    CONSTRAINT warehouseSale_id_pkey PRIMARY KEY (id),
+    CONSTRAINT warehouseSale_warehouseId_fkey FOREIGN KEY (warehouse_id)
+        REFERENCES warehouse (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.sale_products_count
+CREATE TABLE IF NOT EXISTS sale_products_count
 (
     product_sale_id bigint NOT NULL,
     count integer,
     product_id bigint NOT NULL,
-    CONSTRAINT fk7hrfc0jaenaf9ls229s50semv FOREIGN KEY (product_sale_id)
-        REFERENCES public.warehouse_sale (id) MATCH SIMPLE
+    CONSTRAINT saleProductsCount_productSaleId_fkey FOREIGN KEY (product_sale_id)
+        REFERENCES warehouse_sale (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT fkn520q9xjo1nbdwyl3fib3q5an FOREIGN KEY (product_id)
-        REFERENCES public.product (id) MATCH SIMPLE
+    CONSTRAINT saleProductsCount_productId_fkey FOREIGN KEY (product_id)
+        REFERENCES product (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.warehouse_transportation
+CREATE TABLE IF NOT EXISTS warehouse_transportation
 (
     id bigint NOT NULL DEFAULT nextval('warehouse_transportation_id_seq'::regclass),
     create_date timestamp without time zone,
     warehouse_from_id bigint,
     warehouse_to_id bigint,
-    CONSTRAINT warehouse_transportation_pkey PRIMARY KEY (id),
-    CONSTRAINT fkcuhvjonh2cjtt12kjkmpfvl00 FOREIGN KEY (warehouse_from_id)
-        REFERENCES public.warehouse (id) MATCH SIMPLE
+    CONSTRAINT warehouseTransportation_id_pkey PRIMARY KEY (id),
+    CONSTRAINT warehouseTransportation_warehouseFromId_fkey FOREIGN KEY (warehouse_from_id)
+        REFERENCES warehouse (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT fkrgipjn1qoxjaw8gveb69lphy9 FOREIGN KEY (warehouse_to_id)
-        REFERENCES public.warehouse (id) MATCH SIMPLE
+    CONSTRAINT warehouseTransportation_warehouseToId_fkey FOREIGN KEY (warehouse_to_id)
+        REFERENCES warehouse (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS public.transportation_products_count
+CREATE TABLE IF NOT EXISTS transportation_products_count
 (
     product_transportation_id bigint NOT NULL,
     count integer,
     product_id bigint NOT NULL,
-    CONSTRAINT fk11fy8n9wxcitjukqu02b324he FOREIGN KEY (product_transportation_id)
-        REFERENCES public.warehouse_transportation (id) MATCH SIMPLE
+    CONSTRAINT transportationProductsCount_productTransportationId_fkey FOREIGN KEY (product_transportation_id)
+        REFERENCES warehouse_transportation (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT fk18bqk926pw7o6te0tixp28sl7 FOREIGN KEY (product_id)
-        REFERENCES public.product (id) MATCH SIMPLE
+    CONSTRAINT transportationProductsCount_productId_fkey FOREIGN KEY (product_id)
+        REFERENCES product (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
-
